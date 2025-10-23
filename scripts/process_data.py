4560e942cad17fd31da405d65317dfe59d3a50ad
saccini <s09123511@gmail.com>
2025-10-23 19:13:08 +0200

    1. add pseudo.txt and input code to process_data.py

---
+++ scripts/process_data.py
@@ -0,0 +1,8 @@
+import json
+
+with open('data/output.json', 'r') as file:
+    dataset = json.load(file)
+
+movie_genre = input("What genre do you like watch today? ").str.lower()
+
+movie_duration = input("How much time you wish to spare? ")
